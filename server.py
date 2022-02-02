70c58e284fb8c00afe8bbca0639b28b1eb084d56
nnbrandon <nnbrandon@icloud.com>
2022-02-02 00:57:59 -0800

    REST APIs for chart data

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import cross_origin
 from tickerService import TickerService
+import json
 
 # Initialize Flask
 app = Flask(__name__)
@@ -23,6 +24,37 @@ def get_tickers():
       "symbol": tickers[i]['Symbol'],
       "companyName": tickers[i]['Name'],
       "sector": tickers[i]['Sector']
+    })
+
+  return jsonify(json_array)
+
+@app.route("/api/v1/chartdata", methods=['GET'])
+@cross_origin()
+def get_chart_data():
+  ticker = request.args.get('ticker')
+  refresh = json.loads(request.args.get('refresh').lower())
+  period1 = request.args.get('period1')
+  period2 = request.args.get('period2')
+
+  if refresh and not period1 and not period2:
+    return jsonify({
+      "error": "Date periods are required when refreshing the chart data for ticker " + ticker 
+    })
+
+  file_name = 'Data/Tickers/' + ticker + '.csv'
+  ticker_service = TickerService(file_name)
+  chart_data = ticker_service.get_chart_data(ticker, refresh, period1, period2)
+  json_array = []
+
+  for i in range(1, len(chart_data)):
+    json_array.append({
+      "date": chart_data[i]['Date'],
+      "open": chart_data[i]['Open'],
+      "high": chart_data[i]['High'],
+      "low": chart_data[i]['Low'],
+      "close": chart_data[i]['Close'],
+      "adj_close": chart_data[i]['Adj Close'],
+      "volume": chart_data[i]['Volume']
     })
 
   return jsonify(json_array)
--- tickerService.py
+++ tickerService.py
@@ -1,4 +1,10 @@
 import csv
+import math
+from os.path import exists
+import time
+import datetime
+
+from utils import pull_historical_data
 
 class TickerService:
     def __init__(self, file_name):
@@ -8,5 +14,25 @@ class TickerService:
         with open(self.file_name) as f:
             reader = csv.DictReader(f)
             tickers = list(reader)
-
             return tickers
+
+    def get_chart_data(self, ticker, refresh: bool, period1, period2):
+        if refresh:
+            print("Refreshing chart data for " + ticker)
+            pull_historical_data(ticker, period1, period2)
+        elif not exists(self.file_name):
+            print("Chart data does not exist for ticker " + ticker)
+            today_date = time.time()
+            today_date = datetime.datetime.now()
+            six_months_back = today_date - datetime.timedelta(days=183)
+            
+            period1 = math.floor(six_months_back.timestamp())
+            period2 = math.ceil(today_date.timestamp())
+            print(period1)
+            print(period2)
+            pull_historical_data(ticker, period1, period2)
+
+        with open(self.file_name) as f:
+            reader = csv.DictReader(f)
+            chart_data = list(reader)
+            return chart_data
